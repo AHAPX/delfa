3a561f52a7657e1b79a65f2b78ad5c030c9c4370
furashcka <furashcka@mail.ru>
2017-03-04 14:15:29 +0300

    Последний утвержденный макет

--- js/app.js
+++ js/app.js
@@ -1,106 +1,101 @@
-;(function() {
-    function updateHeightLR() {
-        var r = $('.content-960_with-padding-1 .right-block');
-        var l = $('.content-960_with-padding-1 .left-block');
+;(function($) {
+    var flex = {
+        isSupport: function() {
+            var test = document.createElement('test');
+            test.style.display = 'flex';
+            return test.style.display === 'flex';
+        },
+        emulate: function(content, contentItems, fustify, update) {
+            var timeOut = null;
 
-        r.attr('style', '');
-        l.attr('style', '');
+            function init() {
+                if(update !== 'true' && content.data('flexbox') === 'emulate') return;
+                content.data('flexbox', 'emulate');
 
-        var rightH = r.outerHeight(); 
-        var leftH = l.outerHeight(); 
+                content[0].removeAttribute('style');
+                contentItems.each(function() {
+                    $(this)[0].removeAttribute('style');
+                });
 
-        if(rightH > leftH)
-            l.height(rightH-40);
-    }
-    function initMap() {
-        if($('.map__insert').length < 1) return;
-        
-        var map = new GMaps({
-            el: '.map__insert',
-            lat: 55.7558,
-            lng: 37.6173
-        });
+                window.clearTimeout(timeOut);
+                timeOut = window.setTimeout(function() {
+                    var contentWidth = content.outerWidth();
+                    var contentLength = contentItems.length;
+                    var freeSpace = contentWidth;
+                    var padding = 0;
+
+                    contentItems.each(function() {
+                        freeSpace -= $(this).outerWidth();
+                    });
+
+                    if(fustify === true) {
+                        padding = (freeSpace / contentLength) / 2;
+                        padding += padding / (contentLength-1);
+                    }
+                    else {
+                        padding = (freeSpace / contentLength) / 2;
+                    }
+
+                    contentItems.css({
+                        'padding-left': padding,
+                        'padding-right': padding,
+                    });
+                }, 100);
+            }
+
+            init();
+        }
+    };
+    function emulateFlex() {
+        if(window.innerWidth < 960) return;
+
+        $('.footer__nav, .header__nav ul').addClass('not-support-flexbox');
+        flex.emulate($('.footer__nav'), $('.footer__nav > li'), true);
+        flex.emulate($('.header__nav ul'), $('.header__nav ul > li > a'));
     }
     function detectIE() {
         var ua = window.navigator.userAgent;
 
         var msie = ua.indexOf('MSIE ');
         if (msie > 0) {
-            // IE 10 or older => return version number
             return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
         }
 
         var trident = ua.indexOf('Trident/');
         if (trident > 0) {
-            // IE 11 => return version number
             var rv = ua.indexOf('rv:');
             return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
         }
 
         var edge = ua.indexOf('Edge/');
         if (edge > 0) {
-           // Edge (IE 12+) => return version number
            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
         }
 
-        // other browser
         return false;
     }
-    function updateFlexIE(warpElem, contentElem, n) {
-        warpElem.attr('style', '');
-        contentElem.attr('style', '');
-
-        var warpWidth       = warpElem.outerWidth();
-        var itemLength      = contentElem.length;
-        var contentWidth    = 0;
-        var padding         = 0;
-
-        contentElem.each(function(i, el) {
-
-            
-            contentWidth += parseInt($(el).outerWidth());
-        });
-
-        padding = (100 * (warpWidth - contentWidth) / warpWidth) / itemLength;
-
-        warpElem.width(warpWidth);
-        contentElem.css({
-            'padding-left': padding / n + '%',
-            'padding-right': padding / n + '%'
-        });
+    function toggleCourses() {
+        var id = '#' + $('.courses__tabs li.active:first').data('courses');
+        $('.courses__list').hide();
+        $(id).show();
     }
-    function addIEClass(ie) {
-       $('html').addClass('ie ie-' + ie);
-    };
-    function coursesTabs() {
-        var items = $('.courses-tabs__tabs li')
-        items.click(function() {
-            var id = $(this).data('id');
-            items.removeClass('active');
-            $(this).addClass('active');
+    function updateContactsSideHeight() {
+        var sideRight = $('.contacts .side-right');
+        var sideLeft = $('.contacts .side-left');
 
-            $('.courses-content').hide();
-            $('#' + id).show();
-        });
+        sideRight.height(sideLeft.outerHeight());
     }
+
+
+
+    var ieVersion = detectIE();
+    if(ieVersion !== false && ieVersion < 11) {
+        $('html').addClass('ie ie-' + ieVersion);
+    }
+
     $(document).ready(function() {
-        var ieVersion = detectIE();
-        if(document.documentMode || /Edge/.test(navigator.userAgent))
-            ieVersion = 9;
-
-        if(ieVersion !== false && ieVersion < 11) {
-            addIEClass(ieVersion);
-            var idInterval = setInterval(function() {
-                if($('html').hasClass('ie')) {
-                    clearInterval(idInterval);
-                    updateFlexIE($('.nav__header ul'), $('.nav__header ul > li'), 1.99999);
-                    updateFlexIE($('.footer__nav'), $('.footer__nav > li'), 1.89999);
-                }
-            }, 100);
-        }
-
         if($.fn.owlCarousel) {
-            $('.page-content .owl-carousel').owlCarousel({
+            $('.content-article .owl-carousel').owlCarousel({
                 loop:true,
                 margin:10,
                 nav:true,
@@ -117,24 +112,36 @@
                     }
                 }
             });
-        };
+        }
 
-        $('.buy-form .btn_green').click(function(e) {
+        $('.content-article .side-right .join-course-form .btn_green').click(function(e) {
             e.preventDefault();
 
+            $(this).parent().children('*').css('display', 'block');
             $(this).hide();
-            $('.buy-form__hidden').show();
-            $('.buy-form input:first').focus();
+            $(this).parent().children('input:first').focus();
         });
 
-
-        $('.right-page-toggle').click(function() {
-            $('.right-page-toggle, .right-page').toggleClass('active');
+        $('.courses__tabs li').click(function() {
+            $('.courses__tabs li').removeClass('active');
+            $(this).addClass('active');
+            toggleCourses();
         });
+        toggleCourses();
 
-        coursesTabs();
-        initMap();
-        updateHeightLR();
-        $(window).resize(updateHeightLR);
+        $(window).resize(updateContactsSideHeight);
+
+        $('.side-right__toggle').click(function() {
+            $(this).parent().toggleClass('active');
+        });
     });
-})();
+
+    $(window).on("load", function() {
+        updateContactsSideHeight();
+        
+       if(flex.isSupport()) return;
+
+       emulateFlex();
+       $(window).resize(emulateFlex);
+    });
+})(jQuery);
